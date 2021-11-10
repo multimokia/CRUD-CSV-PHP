8690f2c7d14bf24cfea60dde82e40e2f4120ae5e
multimokia <multimokia3@gmail.com>
2021-11-10 17:14:13 -0500

    more groundwork + complete filewrapper

--- functions.inc.php
+++ functions.inc.php
@@ -14,13 +14,55 @@
         $file = fopen($file, $mode);
 
         //Execute func
-        $callable($file);
+        $result = $callable($file);
 
         //Dispose file
         fclose($file);
+
+        //If the work function returned anything, we should return it
+        return $result;
     }
 
-    //Helper function for
+
+    //Helper function for filtering
+    function filter_records($data, $filters)
+    {
+        $data = array_filter(
+            $data,
+            // Build a func that checks if we don't meet a certain criteria. If we don't make it, then we exclude
+            function($record) use ($filters) {
+                foreach ($filters as $filter_key => $filter_val)
+                {
+                    if (!in_array($record[$filter_key], $filter_val))
+                        { return false; }
+                }
+                return true;
+            }
+        );
+        // Return the filtered values
+        return $data;
+    }
+
+    //Dropdown maker
+    function select($name, $options, $selected_choice = null)
+    {
+        $rv = "<select name='$name' id='$name'>";
+
+        foreach ($options as $option_name)
+        {
+            $rv .= "<option value='$option_name'";
+
+            if ($selected_choice == $option_name)
+                { $rv .= " selected"; }
+
+            $rv .= ">$option_name</option>";
+        }
+
+        $rv .= "</select>";
+
+        echo $rv;
+    }
+
     function create(
         $type,
         $brand,
@@ -40,6 +82,21 @@
     }
 
     //READ
+    function get_all_records()
+    {
+        $rv = array();
+
+        open_file_context_manager(
+            "./data/records.csv", "r",
+            function($file) {
+                global $rv;
+                $rv = fgetcsv($file, 1024);
+            }
+        );
+
+        return $rv;
+    }
+
     //Finds a record by id. Returns the record if found or null if not.
     function find_single_record($id)
     {
